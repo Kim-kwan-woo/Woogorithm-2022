066cf4cc6066968d6142c5749906da570ad2d42f
Kim-kwan-woo <kw2577@naver.com>
2022-01-09 13:58:46 +0900

    baekjoon/수학/A+B - 3

---
+++ baekjoon/수학/10950.py
@@ -0,0 +1,14 @@
+import sys
+
+def solution(A, B):
+    return A + B
+
+if __name__ == "__main__":
+    T = int(sys.stdin.readline().rstrip())
+    N = []
+    for i in range(T):
+        A, B = map(int, sys.stdin.readline().split())
+        N.append(solution(A, B))
+
+    for i in N:
+        print(i)
