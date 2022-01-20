12cd6966de291e047ae510c815dfe2f06ad31ef4
Kim-kwan-woo <kw2577@naver.com>
2022-01-20 19:19:54 +0900

    baekjoon/정렬/좌표 정렬하기2

---
+++ baekjoon/단계별/정렬/11651.py
@@ -0,0 +1,14 @@
+import sys
+
+def solution(points):
+    return sorted(points, key= lambda x: (x[1], x[0]))
+
+
+        
+
+if __name__ == "__main__":
+    N = int(sys.stdin.readline().rstrip())
+    points = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+
+    for i in solution(points):
+        print(i[0], i[1])
