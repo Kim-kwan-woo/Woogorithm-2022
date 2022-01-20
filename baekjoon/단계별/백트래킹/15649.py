3933fba011f8d6b20884ef59a8acd671b5bc5cc7
Kim-kwan-woo <kw2577@naver.com>
2022-01-20 22:05:27 +0900

    baekjoon/백트래킹/N과 M(1)

---
+++ baekjoon/단계별/백트래킹/15649.py
@@ -0,0 +1,23 @@
+import sys
+
+def solution(numList, M, num):
+    if M == 0:
+        print(num)
+    else:
+        for i in range(len(numList)):
+            if num == '':
+                solution(numList[:i]+numList[i+1:], M-1, str(numList[i]))
+            else:
+                solution(numList[:i]+numList[i+1:], M-1, num +" "+ str(numList[i]))
+
+            
+
+
+
+
+        
+
+if __name__ == "__main__":
+    N, M = map(int,sys.stdin.readline().split())
+    numList = [i for i in range(1, N+1)]
+    solution(numList, M, '')
