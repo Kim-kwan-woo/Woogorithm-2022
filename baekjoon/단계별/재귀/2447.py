de47f38b5eeadaf81a30b0095726c1deb21e6928
Kim-kwan-woo <kw2577@naver.com>
2022-01-24 13:28:20 +0900

    baekjoon/분할 정복,재귀/별찍기 - 10 (메모리초과)

--- baekjoon/단계별/재귀/2447.py
+++ baekjoon/단계별/재귀/2447.py
@@ -1,16 +1,41 @@
 import sys
+# 재귀 함수로 구현(메모리 초과 발생)
+# def solution(N, star, count):
+#     stars = []
+#     for i in range(len(star) * 3):
+#         if i // len(star) == 1:
+#             stars.append(star[i % len(star)] + " " * len(star) + star[i%len(star)])
+#         else:
+#             stars.append(star[i % len(star)] * 3)
 
-def solution(N, count, star):
-    nextStar = star*4 + ' '*len(star) + star*4
-    if count < int(N**(1/3)):
-        return solution(N, count+1, nextStar)
-    else:
-        return nextStar
+#     if count == int(N**(1/3))-1:
+#         for j in stars:
+#             print(j)
+#     else:  
+#         solution(N, stars, count+1)
+
+#비 재귀 함수로 구현(메모리 초과 발생)
+def solution(N, star):
+    count = 0
+    while count != int(N**(1/3))-1:
+        stars = []
+        for i in range(len(star) * 3):
+            if i // len(star) == 1:
+                stars.append(star[i % len(star)] + " " * len(star) + star[i%len(star)])
+            else:
+                stars.append(star[i % len(star)] * 3)
+
+        star = stars
+        count += 1
+
+    for i in star:
+        print(i)
     
     
-
 
 if __name__ == "__main__":
     N = int(sys.stdin.readline().rstrip())
-    star = solution(N, 1, '*')
-    start = 3**(int(N**(1/3))-1)
+    star = ["***", "* *", "***"]
+
+    solution(N, star)
+
