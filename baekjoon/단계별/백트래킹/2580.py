ef89b8b76a16d5e1cbf31757621c3e352995e0da
Kim-kwan-woo <kw2577@naver.com>
2022-01-25 14:14:07 +0900

    baekjoon/백트래킹/스도쿠

--- baekjoon/단계별/백트래킹/2580.py
+++ baekjoon/단계별/백트래킹/2580.py
@@ -1,40 +1,52 @@
 import sys
 
-def solution():
-    #가로 비교
+def solution(count):
+    if count == len(zero):
+        for i in range(9):
+            #리스트 앞에 *을 쓰면 모든 원소가 공백으로 구분되어 문자열 형태로 출력된다.
+            print(*sudoku[i])
+        # 답이 여러개가 나올 수 있으므로 return이 아닌 exit를 사용하여 하나의 답만 출력 후 프로그램 종료
+        exit(0)
+
+    for i in range(1, 10):
+        x = zero[count][0]
+        y = zero[count][1]
+
+        if checkRow(x, i) and checkCol(y, i) and checkRect(x, y, i):
+            sudoku[x][y] = i
+            solution(count+1)
+            #백트래킹(다른 경로의 값들을 위해 값을 초기화 해주어야 한다.)
+            sudoku[x][y] = 0 
+
+def checkRow(x, num):
     for i in range(9):
-        if sudoku[i].count(0) == 1:
-            index = 0
-            numList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-            for j in range(9):
-                numList.remove(sudoku[i][j])
-                if sudoku[i][j] == 0:
-                    index = j
+        if num == sudoku[x][i]:
+            return False
+    return True
 
-            sudoku[i][index] = numList[0]
-
-    #세로 비교(아직 안했음)
+def checkCol(y, num):
     for i in range(9):
-        if sudoku[i][i].count(0) == 1:
-            index = 0
-            numList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-            for j in range(9):
-                numList.remove(sudoku[i][j])
-                if sudoku[i][j] == 0:
-                    index = j
+        if num == sudoku[i][y]:
+            return False
+    return True
 
-            sudoku[i][index] = numList[0]
-            
+def checkRect(x, y, num):
+    tempX = x // 3 * 3
+    tempY = y // 3 * 3
+    for i in range(tempX, tempX + 3):
+        for j in range(tempY, tempY + 3):
+            if num == sudoku[i][j]:
+                return False
+    return True
 
-
-
-        
 
 if __name__ == "__main__":
-    #이거 스도쿠 1차원 배열로 해봐야겠다.
     sudoku = [list(map(int,sys.stdin.readline().split())) for _ in range(9)]
-    solution()
+
+    zero = []
     for i in range(9):
-        print()
         for j in range(9):
-            print(sudoku[i][j], end=' ')
+            if sudoku[i][j] == 0:
+                zero.append((i, j))
+
+    solution(0)
