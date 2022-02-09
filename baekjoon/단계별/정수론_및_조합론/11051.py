0ccd5d9abd946def5bbad8cc7fb90c5b34c6381c
Kim-kwan-woo <kw2577@naver.com>
2022-02-09 15:04:07 +0900

    baekjoon/수학,다이나믹 프로그래밍,조합론/이항 계수 2

---
+++ baekjoon/단계별/정수론_및_조합론/11051.py
@@ -0,0 +1,15 @@
+import sys
+
+def solution():
+    multi = [1]*(N+1)
+
+    for i in range(1, N+1):
+        multi[i] = multi[i-1]*i
+
+    return (multi[N]//(multi[K]*multi[N-K])) % 10007
+
+if __name__ == "__main__":
+    N, K = map(int, sys.stdin.readline().split())
+    
+    print(solution())
+    
