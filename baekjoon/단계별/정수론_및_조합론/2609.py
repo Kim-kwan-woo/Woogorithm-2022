10127db5b4c482092f0becf8768411f4c681b84e
Kim-kwan-woo <kw2577@naver.com>
2022-02-09 14:32:28 +0900

    baekjoon/유클리드 호제법 사용/최대공약수와 최소공배수

--- baekjoon/단계별/정수론_및_조합론/2609.py
+++ baekjoon/단계별/정수론_및_조합론/2609.py
@@ -4,15 +4,28 @@ def solution():
     minNum = min(num1, num2)
     maxNum = max(num1, num2)
     gcd, lcm = 0, 0
-    for i in range(minNum, 0, -1):
-        if num1 % i == 0 and num2 % i == 0:
-            gcd = i
-            break
 
-    for i in range(1, minNum+1):
-        if (maxNum * i) % minNum == 0:
-            lcm = maxNum * i
-            break
+    #최대공약수
+    # for i in range(minNum, 0, -1):
+    #     if num1 % i == 0 and num2 % i == 0:
+    #         gcd = i
+    #         break
+
+    #최소공배수
+    # for i in range(1, minNum+1):
+    #     if (maxNum * i) % minNum == 0:
+    #         lcm = maxNum * i
+    #         break
+
+    #유클리드 호제법(최대 공약수)
+    while minNum != 0:
+        mod = maxNum % minNum
+        maxNum = minNum
+        minNum = mod
+    gcd = maxNum
+
+    #유클리드 호제법(최소 공배수)
+    lcm = (num1*num2) // gcd
     
     return (gcd, lcm)
 
