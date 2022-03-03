4a49e9bf3afd6d511a4be79b03d7ce6ccf9d1a32
Kim-kwan-woo <kw2577@naver.com>
2022-03-03 19:43:59 +0900

    이것이취업을위한코딩테스트다/그리디/1이 될 때까지

---
+++ 이것이취업을위한코딩테스트다/그리디/3-4.py
@@ -0,0 +1,36 @@
+import sys
+
+#---answer---#
+N, K = map(int, sys.stdin.readline().split())
+
+count = 0
+while N != 1:
+    if N % K == 0:
+        N //= K
+    else:
+        N -= 1
+
+    count += 1
+
+print(count)
+
+#---solution---#
+# # N, K를 공백으로 구분하여 입력받기
+# n, k = map(int, input().split())
+# result = 0
+
+# while True:
+#     # (N == K로 나누어 떨어지는 수)가 될 때까지 1씩 빼기
+#     target = (n//k) * K
+#     result += (n-target)
+#     n = target
+#     # N이 K보다 작을 때(더 이상 나눌 수 없을 때) 반복문 탈출
+#     if n < k:
+#         break
+#     # K로 나누기
+#     result += 1
+#     n //= K
+
+# # 마지막으로 남은 수에 대하여 1씩 빼기
+# result += (n-1)
+# print(result)
