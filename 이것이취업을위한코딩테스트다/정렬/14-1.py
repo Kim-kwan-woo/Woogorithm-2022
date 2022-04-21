55cb7110b1c9a48a102e62bb387df3a086107c69
Kim-kwan-woo <kw2577@naver.com>
2022-04-21 20:37:20 +0900

    이것이취업을위한코딩테스트다/정렬/국영수

---
+++ 이것이취업을위한코딩테스트다/정렬/14-1.py
@@ -0,0 +1,29 @@
+import sys
+
+#[baekjoon 14888 연산자 끼워 넣기]
+#---answer---#
+N = int(sys.stdin.readline().rstrip()) # 학생의 수
+student = []
+
+for i in range(N):
+    data = sys.stdin.readline().split()
+    student.append([data[0], int(data[1]), int(data[2]), int(data[3])])
+
+student.sort(key=lambda x:(-x[1], x[2], -x[3], x[0]))
+
+for i in student:
+    print(i[0])
+
+#---solution---#
+# n = int(input())
+# students = [] # 학생 정보를 담을 리스트
+
+# # 모든 학생 정보를 입력받기
+# for _ in range(n):
+#     students.append(input().split())
+
+# student.sort(key=lambda x: (-int(x[1]), int(x[2]), -int(x[3]), x[0]))
+
+# # 정렬된 학생 정보에서 이름만 출력
+# for student in students:
+#     print(student[0])
